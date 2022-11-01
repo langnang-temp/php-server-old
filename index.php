e93815ecbf5546bfa04fe4737cda107ac9e328e5
langnang <langnang.chen@outlook.com>
2022-11-02 01:15:17 +0800

    rewrite

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
+$rewrite = "/?";
 
-$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
-  $r->addRoute('GET', '/', function ($vars) {
-    print_r($vars);
-  });
-  $r->addRoute('GET', '/users', function ($vars) {
-    print_r("users");
-  });
-  // {id} must be a number (\d+)
-  $r->addRoute('GET', '/user/{id:\d+}', function ($vars) {
-    print_r($vars);
-  });
-  // The /{title} suffix is optional
-  $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', function ($vars) {
-    print_r($vars);
+$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $router) use ($rewrite) {
+  $router->addGroup($rewrite, function (FastRoute\RouteCollector $router) {
+    $router->addRoute('GET', '/', function ($vars) {
+      print_r($vars);
+    });
+    $router->addRoute('GET', '/users', function ($vars) {
+      print_r("users");
+    });
+    // {id} must be a number (\d+)
+    $router->addRoute('GET', '/user/{id:\d+}', function ($vars) {
+      print_r($vars);
+    });
+    // The /{title} suffix is optional
+    $router->addRoute('GET', '/articles/{id:\d+}[/{title}]', function ($vars) {
+      print_r($vars);
+    });
   });
 });
 
@@ -24,9 +27,10 @@ $httpMethod = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
 
 // Strip query string (?foo=bar) and decode URI
-if (false !== $pos = strpos($uri, '?')) {
+if (false !== $pos = strpos(substr($uri, strlen($rewrite)), '?')) {
   $uri = substr($uri, 0, $pos);
 }
+
 $uri = rawurldecode($uri);
 
 $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
