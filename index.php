7de0f0a99abfbacd5b4a02b1951ac901313bfec7
langnang <langnang.chen@outlook.com>
2022-11-02 01:06:23 +0800

    nikic/fast-route

--- index.php
+++ index.php
@@ -1,43 +1,49 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-?>
 
-<!doctype html>
-<html>
 
-<head>
-  <meta charset="utf-8">
-  <title>恭喜，站点创建成功！</title>
-  <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css">
-  <script src="//cdn.bootcss.com/jquery/1.11.2/jquery.min.js"></script>
-  <script src="//cdn.bootcss.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-</head>
+$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
+  $r->addRoute('GET', '/', function ($vars) {
+    print_r($vars);
+  });
+  $r->addRoute('GET', '/users', function ($vars) {
+    print_r("users");
+  });
+  // {id} must be a number (\d+)
+  $r->addRoute('GET', '/user/{id:\d+}', function ($vars) {
+    print_r($vars);
+  });
+  // The /{title} suffix is optional
+  $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', function ($vars) {
+    print_r($vars);
+  });
+});
 
-<body>
-  <div class="container" style="margin-top:9%;">
-    <div class="jumbotron">
-      <div class="panel panel-success">
-        <div class="panel-heading">
-          <h1>恭喜, 站点创建成功！</h1>
-        </div>
-      </div>
-      <p>
-      <h3>站点创建成功，本页面由系统自动生成。</h3>
-      </p>
-      <p>
-        <li>本页面在FTP根目录下的index.html</li>
-      </p>
-      <p>
-        <li>您可以删除或覆盖本页面</li>
-      </p>
-      <p>
-        <li>FTP相关信息，请到宝塔后台查看</li>
-      </p>
-      <p>
-        <li>我们为您提供了完善的数据备份功能，请到后台进行相关设置21</li>
-      </p>
-    </div>
-  </div>
-</body>
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
 
-</html>
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+  $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+  case FastRoute\Dispatcher::NOT_FOUND:
+    die("404 Not Found");
+    // ... 404 Not Found
+    break;
+  case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+    $allowedMethods = $routeInfo[1];
+    die("405 Method Not Allowed");
+    // ... 405 Method Not Allowed
+    break;
+  case FastRoute\Dispatcher::FOUND:
+    $handler = $routeInfo[1];
+    $vars = $routeInfo[2];
+    // ... call $handler with $vars
+    $handler($vars);
+    break;
+}
