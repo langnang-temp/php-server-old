d909ad33527d303d8e4b3fd29edc3c0dfe4d49c2
langnang <langnang.chen@outlook.com>
2022-11-02 23:24:26 +0800

    directory structure

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/src/utils/main.php';
+require_once __DIR__ . '/src/modules/main.php';
+
 
 // 伪静态
 $rewrite = "/?";
@@ -14,109 +17,11 @@ $log->error('Bar');
 
 $dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $router) use ($rewrite) {
   $router->addGroup($rewrite, function (FastRoute\RouteCollector $router) {
-    $router->addRoute('GET', '/', function ($vars) {
-      print_r($vars);
-    });
-    $router->addRoute('GET', '/users', function ($vars) {
-      print_r("users");
-    });
-    // {id} must be a number (\d+)
-    $router->addRoute('GET', '/user/{id:\d+}', function ($vars) {
-      print_r($vars);
-    });
-    // The /{title} suffix is optional
-    $router->addRoute('GET', '/articles/{id:\d+}[/{title}]', function ($vars) {
-      print_r($vars);
-    });
-    // logger
-    $router->addRoute('GET', '/logger', function ($vars) {
-      $content = explode("\n", file_get_contents(__DIR__ . '/.log'));
-      foreach ($content as $v) {
-        echo $v . '<br/>';
-      }
-    });
-    // configs
-    $router->addRoute('GET', '/install/{host}/{dbname}/{user}/{password}', function ($vars) {
-      $content = "<?php 
-return array(
-  'host' => '{$vars['host']}',
-  'dbname' => '{$vars['dbname']}',
-  'user' => '{$vars['user']}',
-  'password' => '{$vars['password']}',
-  'driver' => 'pdo_mysql',
-);      
-";
-      file_put_contents(__DIR__ . '/config.inc.php', $content);
-      $_CONFIG = require_once(__DIR__ . '/config.inc.php');
-      var_dump($_CONFIG);
-    });
-    // mysql connection
-    $router->addRoute('GET', '/conn', function ($vars) {
-      $_CONFIG = require_once(__DIR__ . '/config.inc.php');
-      $conn = \Doctrine\DBAL\DriverManager::getConnection($_CONFIG);
-      $rows = $conn->fetchAllAssociative("SHOW TABLES");
-      var_dump($rows);
-    });
-    $router->addRoute('GET', '/monolog-mysql', function ($vars) {
-      $_CONFIG = require_once(__DIR__ . '/config.inc.php');
-      $conn = \Doctrine\DBAL\DriverManager::getConnection($_CONFIG);
-      $sql_create_table = "CREATE TABLE IF NOT EXISTS `log` (
-        id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, channel VARCHAR(255), level INTEGER, message LONGTEXT, time INTEGER UNSIGNED, INDEX(channel) USING HASH, INDEX(level) USING HASH, INDEX(time) USING BTREE
-    )";
-      $conn->executeQuery($sql_create_table);
-      $pdo = new PDO("mysql" . ":dbname=" . $_CONFIG["dbname"] . ";host=" . $_CONFIG["host"], $_CONFIG["user"], $_CONFIG["password"]);
-
-      $mySQLHandler = new MySQLHandler\MySQLHandler($pdo, "log", array('var', 'result', 'uuid', 'timestamp'), \Monolog\Logger::DEBUG);
-
-      //Create logger
-      $logger = new \Monolog\Logger("monolog-mysql");
-      $logger->pushHandler($mySQLHandler);
-
-      //Now you can use the logger, and further attach additional information
-      $logger->warning("This is a great message, woohoo!", array('var'  => 'var', 'result'  => 'result', 'uuid'  => 'uuid', 'timestamp'  => 'timestamp'));
-
-      $sql_select_list = "SELECT * FROM `log` ORDER BY `id` DESC LIMIT 10 ";
-      $rows = $conn->fetchAllAssociative($sql_select_list);
-      var_dump($rows);
-    });
-    // try-catch
-    $router->addRoute('GET', '/try-catch', function ($vars) {
-      try {
-        throw new Exception("test try-catch exception.");
-        exit;
-      } catch (Exception $error) {
-        echo $error->getMessage();
-      }
-    });
-    // swagger-php
-    $router->addRoute('GET', '/swagger-php', function ($vars) {
-      $openapi = \OpenApi\Generator::scan(['swagger/examples']);
-      header('Content-Type: application/json');
-      echo $openapi->toJson();
-    });
-
-    // faker
-    $router->addRoute('GET', '/faker/{method}', function ($vars) {
-      $_FAKER = Faker\Factory::create();
-      $method = $vars['method'];
-      print_r(["method" => $method, "value" => $_FAKER->{$vars['method']}()]);
-    });
-
-    $router->addRoute("GET", "/request", function ($vars) {
-      // if (!isset($vars['url'])) return new Exception("no url specified.");
-      // $url = $vars['url'];
-      $url = 'https://inshorts.deta.dev/news?category=science';
-      $method = strtolower($vars['method']);
-      $headers = isset($vars['headers']) ? (array)$vars['headers'] : [];
-      $data = isset($vars['data']) ? (array)$vars['data'] : [];
-      if (!in_array($method, ['get', 'post', 'put', 'delete'])) $method = 'get';
-      $response = WpOrg\Requests\Requests::$method($url, $headers, $data);
-      $body = json_decode($response->body, true);
-      if (!is_null($body)) $response->body = $body;
-      var_dump($response);
-    });
+    require_once __DIR__ . '/src/apis/main.php';
+    require_once __DIR__ . '/src/routes/main.php';
   });
 });
+
 
 
 // Fetch method and URI from somewhere
