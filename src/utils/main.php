f9de07160ba06a4af295cd2c9f42f7d54f8301c6
langnang <langnang.chen@outlook.com>
2022-11-03 16:30:49 +0800

    auto require

--- src/utils/main.php
+++ src/utils/main.php
@@ -9,3 +9,22 @@ function timestamp(): int
   list($microsecond, $time) = explode(' ', microtime()); //' '中间是一个空格
   return (float)sprintf('%.0f', (floatval($microsecond) + floatval($time)) * 1000);
 }
+
+
+/**
+ * 遍历加载文件夹下文件
+ */
+function require_path($path, $callback)
+{
+  if (is_dir($path)) {
+    foreach (scandir($path) as $_path) {
+      if (!in_array($_path, ['.', '..'])) {
+        require_path($path . '/' . $_path, $callback);
+      }
+    }
+  } else {
+    if (pathinfo($path)['extension'] === 'php' && $callback(pathinfo($path))) {
+      require_once $path;
+    }
+  }
+}
